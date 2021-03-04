7e0cf06059bfcdc85bfb277f33f1a7a46fba627a
lizhixiang688 <1176590542@qq.com>
2021-03-04 14:36:00 +0800

    12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("111");
+        System.out.println("222");
     }
 }
